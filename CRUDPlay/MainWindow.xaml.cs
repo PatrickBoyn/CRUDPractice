7a5c6fab6b7f27294e1d4746d83b2620109d8c1b
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-02-09 13:21:57 -0500

    Initial Commit.

---
+++ CRUDPlay/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CRUDPlay.Models;
+using SQLite;
+
+namespace CRUDPlay
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ReadDatabase();
+        }
+
+        private void SaveNumberButton_OnClick(object sender, RoutedEventArgs e)
+        {
+             InsertWeight();
+        }
+
+        void InsertWeight()
+        {
+            var weight = new Weight()
+            {
+                Weights = double.Parse(NumberInputTextBox.Text),
+                DateCreated = DateTime.Now
+            };
+
+            using (var conn = new SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Weight>();
+                conn.Insert(weight);
+            }
+        }
+
+        void ReadDatabase()
+        {
+            var weight = new Weight();
+            List<Weight> weights;
+
+            using (var conn = new SQLiteConnection(App.databasePath))
+            {
+                conn.CreateTable<Weight>();
+                weights = conn.Table<Weight>().ToList();
+            }
+
+            if (weights != null)
+                DNumListView.ItemsSource = weights;
+        }
+
+        private void NumberInputTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            double parsedValue;
+
+            SaveNumberButton.IsEnabled = !string.IsNullOrEmpty(NumberInputTextBox.Text) && double.TryParse(NumberInputTextBox.Text, out parsedValue);
+
+            if (SaveNumberButton.IsEnabled)
+            {
+                SaveNumberButton.Background = new SolidColorBrush(Colors.Green);
+                SaveNumberButton.Foreground = new SolidColorBrush(Colors.White);
+            }
+        }
+    }
+}
---
+++ CRUDPlay/Models/Weight.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace CRUDPlay.Models
+{
+    class Weight
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public double Weights { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
