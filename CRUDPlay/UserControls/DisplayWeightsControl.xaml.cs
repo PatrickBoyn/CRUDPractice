7c80e6318f49e566223ccb49986f296320e24107
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-02-16 16:46:53 -0500

    Created a dependency property so I can use the control.

--- CRUDPlay/UserControls/DisplayWeightsControl.xaml.cs
+++ CRUDPlay/UserControls/DisplayWeightsControl.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CRUDPlay.Models;
 
 namespace CRUDPlay.UserControls
 {
@@ -20,6 +21,25 @@ namespace CRUDPlay.UserControls
     /// </summary>
     public partial class DisplayWeightsControl : UserControl
     {
+
+
+
+        public Weight Weight
+        {
+            get { return (Weight)GetValue(WeightProperty); }
+            set { SetValue(WeightProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Weight.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty WeightProperty =
+            DependencyProperty.Register("Weight", typeof(Weight), typeof(DisplayWeightsControl), new PropertyMetadata(new Weight(), DisplayText));
+
+        private static void DisplayText(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as DisplayWeightsControl;
+        }
+
+
         public DisplayWeightsControl()
         {
             InitializeComponent();
